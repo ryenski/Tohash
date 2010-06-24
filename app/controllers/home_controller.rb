011fbb9010e18ebe973e0f9186a859a9fa05b092
Ryan Heneise <ryan@mysmallidea.com>
2010-06-23 23:21:17 -0400

    Get the first hash for the home page example.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @sample_hash = Tuple.find_by_sha2("56bbdda335500fc38e1c10458f2e5865e1927c5978e9b0a3667db67196fc77dc")
+    @sample_hash = Tuple.first
   end
 
 end
